b73a9f1e4a356fe841f9ea9ffa0d14929170ce19
LucasFR-TH <lucas.f.roldan@gmail.com>
2025-05-27 18:42:58 -0300

    comienzo del poyeco 1

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,16 @@
+namespace EspacioCalculadora;
+
+public class Calculadora
+{
+    private double dato;
+
+    public double Resultado
+    {
+        get => dato;
+    }
+
+    public void Sumar(double termino)
+    {
+
+    }
+}
